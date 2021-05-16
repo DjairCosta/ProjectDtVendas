3809d3c0ba4f43ed09eada02d0bd4bd0f3f1a33d
DjairCosta <costadjaircloud@gmail.com>
2021-05-15 23:55:45 -0300

    update project

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/DjairCosta" target="_blank" rel="noreferrer">Djair Costa</a></p>
+                <p className="text-light">My linkedin visit <a href="https://linkedin.com/in/DjairCosta" target="_blank" rel="noreferrer">Djair Costa</a></p>
+                <p className="text-light"><small><strong>My Portfolio Personal</strong><br />
+                  Evento promovido pela empresa DevSuperior: <a href="https://www.instagram.com/djairrobeto/" target="_blank" rel="noreferrer">@djairroberto</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,13 @@
+export type SaleSum = {
+
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+
+    sellerName: string;
+    visited: number;
+    deals: number;
+
+}
